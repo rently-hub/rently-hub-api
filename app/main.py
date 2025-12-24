6d2a766ce563bb80ab8013779f106c3234b108c1
eccarneiro <tenseioficial@gmail.com>
2025-12-24 16:34:03 -0300

    feat: add CORS middleware to support cross-origin requests

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.api import api_router
 from app.db.session import engine
-from app.db.base import Base  # Importante para que os models sejam registados
+from app.db.base import Base  
+from starlette.middleware.cors import CORSMiddleware
 
 # Cria as tabelas no banco de dados (A "migração" automática inicial)
 Base.metadata.create_all(bind=engine)
@@ -10,6 +11,20 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173", 
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
